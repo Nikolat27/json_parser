ec44e5b5f4a001828f2dc4c8750f0ba73b21685e
nikolat27 <samalizadeh899@gmail.com>
2025-04-25 18:35:27 +0330

    Changed the jsonInput

--- main.go
+++ main.go
@@ -7,13 +7,12 @@ import (
 )
 
 func main() {
-	jsonString := `{"dic1": [1,[5, 6, 7],3,4]}`
+	jsonString := `{"arr1": [1,3,4]}`
 
 	tokens := lexer.Lexer(jsonString)
-	fmt.Println(tokens)
 	parsedJson := parser.Parser(tokens)
 	
-	value, err := parsedJson.GetArray("dic1")
+	value, err := parsedJson.GetArray("arr1")
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
